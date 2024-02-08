73c40bde7fe4068b352cca10f2fb5bb130571eeb
Buckwich <Buckwich@users.noreply.github.com>
2024-02-08 22:39:57 +0100

    `yarn add -D prettier` & add config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  printWidth: 120,
+};
+
+export default config;
